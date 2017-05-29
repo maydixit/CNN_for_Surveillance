2232236c2216ba9fe1e9c9ffa1a563585f048455
Utkarsh <cyberutkarsh@gmail.com>
2017-05-29 00:20:58 -0700

    added data augmentation features

--- utilities/extractFramesFromVideo.py
+++ utilities/extractFramesFromVideo.py
@@ -6,20 +6,33 @@ import cv2
 import sys
 
 #initialize
-videoFilePath = sys.argv[0] #absolute path tothe video file
-frameStart = sys.argv[1] #start frame of capture
-frameEnd = sys.argv[2] #end frame of capture
-frameSkip = sys.argv[3] #number of frames to skip between captures
-frameName = sys.argv[4] #name of the frame to save in the output jpg capture
+videoFilePath = sys.argv[1] #absolute path tothe video file
+frameStart = long(sys.argv[2]) #start time of frame of capture (in MS)
+frameEnd = long(sys.argv[3]) #end time of frame of capture (in MS)
+frameSkip = long(sys.argv[4]) #number of frames to skip between captures
+frameName = sys.argv[5] #name of the frame to save in the output jpg capture
+augment = sys.argv[6] # 0 = off and 1 = on (augment the frames)
 
 vidcap = cv2.VideoCapture(videoFilePath)
 
 #Loop and capture
-for i in range(frameStart,frameEnd,frameSkip):
+for i in range(frameStart, frameEnd, frameSkip):
         vidcap.set(0,i)      # cue to required position
         success,image = vidcap.read()
         if success:
-                print("Capturing Frame - "+ str(i))
-                cv2.imwrite("Cap-" + str(frameName) + "-" + str(i) + ".jpg", image)     # save frame as JPEG file
+                print("Capturing Frame - "+ str(i))                                
+                if augment == "1":
+                	vertFlip = cv2.flip(image,1)
+                        cv2.imwrite("Cap-FlippedVert-" + str(frameName) + "-" + str(i) + ".jpg", vertFlip)     # save frame as JPEG file
+                	horizFlip = cv2.flip(image,0)
+                        cv2.imwrite("Cap-FlippedHoriz-" + str(frameName) + "-" + str(i) + ".jpg", horizFlip)     # save frame as JPEG file
+                	grayScale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)                	
+                        cv2.imwrite("Cap-Grayscale-" + str(frameName) + "-" + str(i) + ".jpg", grayScale)     # save frame as JPEG file
+                        #original image
+                        cv2.imwrite("Cap-" + str(frameName) + "-" + str(i) + ".jpg", image)     # save frame as JPEG file
+                else:
+                        print("other")
+                        cv2.imwrite("Cap-" + str(frameName) + "-" + str(i) + ".jpg", image)     # save frame as JPEG file
+
 
 print("Capture completed. Captured " + str(i) + " frames.")
