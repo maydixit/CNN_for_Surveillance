5fd606a6f32b9bafd60b4f23e16d600f2d623d4c
May <cdixit@stanford.edu>
2017-06-03 19:31:56 -0700

    updated classficiation to python 3

--- classification.py
+++ classification.py
@@ -11,37 +11,41 @@ print ("model: ", model)
 def extract_score(line):
         return float(line.split()[3].replace(")", ""))
 
-cmd = "ls " +path 
+cmd = "ls " +path
 proc = subprocess.Popen(cmd, shell=True ,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-files = proc.communicate()[0] 
+files = proc.communicate()[0]
 
+label = ""
 a = 0
 b = 0
 for filename in files.splitlines():
         abandoned = 0
         background = 0
-        
-        filename = filename.decode('ascii')     
-        if filename is "": 
+
+        filename = filename.decode('ascii')
+        if filename is "":
                 continue
-        print (filename) 
         cmd_classify = "python classify.py " + path + filename + " " + model + " labels/bag_detection.txt"
         proc_classify = subprocess.Popen(cmd_classify, shell=True ,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
         lines = proc_classify.communicate()[0].splitlines()
-        print (cmd_classify)
+        #print (cmd_classify)
         for line in lines:
                 line = line.decode('ascii')
                 if 'score' in line:
                         score = extract_score(line)
-                        if 'abandoned' in line: 
+                        if 'abandoned' in line:
                                 abandoned = score
                         else:
                                 background = score
-        if abandoned > background: 
+
+        if abandoned > background:
                 a += 1
-        else: 
+                label = "abandoned"
+        else:
                 b += 1
-        print ("scores: " , abandoned, background)
-        print ("counts: " , a, b )
+                label = "background"
+        print (filename + " classified as: ", label, " scores: " , abandoned, background, " counts so far: ", a, b )
+
+
 print ("Abandoned: " , a )
 print (" Background: ", b)
